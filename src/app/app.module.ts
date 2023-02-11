9c9ac178c5cf306d279a9bee0e7180c0f373ebd2
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-11 16:51:42 -0300

    Adding to-dos list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { AllComponent } from './pages/all/all.component';
 import { TodayComponent } from './pages/today/today.component';
 import { TomorrowComponent } from './pages/tomorrow/tomorrow.component';
@@ -23,6 +24,7 @@ import { NewComponent } from './pages/new/new.component';
     LoginComponent,
     UserCardComponent,
     TabsComponent,
+    TodoListComponent,
     AllComponent,
     TodayComponent,
     TomorrowComponent,
