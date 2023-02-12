944a53e60747c05c57d992eac5833bfa238abf2b
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-12 09:50:59 -0300

    Functionality for Today and Tomorrow pages

--- src/app/pages/today/today.component.ts
+++ src/app/pages/today/today.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
 	selector: 'app-today',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
 	styleUrls: ['./today.component.css']
 })
 export class TodayComponent {
+	public toDos: any[] | null = null;
 
+	constructor(
+		private backend: BackendService,
+		private afAuth: AngularFireAuth
+	) { }
+
+	ngOnInit(): void {
+		this.afAuth.idToken.subscribe(authToken =>
+			this.backend.getTodayToDoItems(authToken)
+				.subscribe((data: any) => this.toDos = data));
+	}
 }
--- src/app/pages/tomorrow/tomorrow.component.ts
+++ src/app/pages/tomorrow/tomorrow.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
 	selector: 'app-tomorrow',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
 	styleUrls: ['./tomorrow.component.css']
 })
 export class TomorrowComponent {
+	public toDos: any[] | null = null;
 
+	constructor(
+		private backend: BackendService,
+		private afAuth: AngularFireAuth
+	) { }
+
+	ngOnInit(): void {
+		this.afAuth.idToken.subscribe(authToken =>
+			this.backend.getTomorrowToDoItems(authToken)
+				.subscribe((data: any) => this.toDos = data));
+	}
 }
