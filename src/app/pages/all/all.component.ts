6055067fd5da62910882fd16e535acc8ca2d8582
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-12 01:20:43 -0300

    Finish All To-Dos page

--- src/app/pages/all/all.component.ts
+++ src/app/pages/all/all.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
 	selector: 'app-all',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
 	styleUrls: ['./all.component.css']
 })
 export class AllComponent {
+	public toDos: any[] | null = null;
 
+	constructor(
+		private backend: BackendService,
+		private afAuth: AngularFireAuth
+	) { }
+
+	ngOnInit(): void {
+		this.afAuth.idToken.subscribe(authToken =>
+			this.backend.getAllToDoItems(authToken)
+				.subscribe((data: any) => this.toDos = data));
+	}
 }
