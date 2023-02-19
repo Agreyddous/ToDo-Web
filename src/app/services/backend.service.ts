f119d108d74eb45c1d6221eeec4ff3c7db4a6f83
NandoGomes <fernandovbmgomes@hotmail.com>
2023-02-19 13:20:39 -0300

    Passing client's current date

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
@@ -17,23 +18,23 @@ export class BackendService {
 	}
 
 	public getTodayToDoItems(authToken: string | null) {
-		return this.requestFromBackend(authToken, 'get', 'ToDoItems/Today', null, { 'isComplete': false });
+		return this._requestFromBackend(authToken, 'get', 'ToDoItems/Today', null, { 'currentDate': this._currentDate(), 'isComplete': false });
 	}
 
 	public getTomorrowToDoItems(authToken: string | null) {
-		return this.requestFromBackend(authToken, 'get', 'ToDoItems/Tomorrow', null, { 'isComplete': false });
+		return this._requestFromBackend(authToken, 'get', 'ToDoItems/Tomorrow', null, { 'currentDate': this._currentDate(), 'isComplete': false });
 	}
 
 	public getAllToDoItems(authToken: string | null) {
-		return this.requestFromBackend(authToken, 'get', 'ToDoItems');
+		return this._requestFromBackend(authToken, 'get', 'ToDoItems');
 	}
 
 	public getAllAvailableToDoItems(authToken: string | null) {
-		return this.requestFromBackend(authToken, 'get', 'ToDoItems/Available');
+		return this._requestFromBackend(authToken, 'get', 'ToDoItems/Available');
 	}
 
 	public getAllHiddenToDoItems(authToken: string | null) {
-		return this.requestFromBackend(authToken, 'get', 'ToDoItems/Hidden');
+		return this._requestFromBackend(authToken, 'get', 'ToDoItems/Hidden');
 	}
 
 	public createToDoItem(
@@ -42,7 +43,7 @@ export class BackendService {
 		description: string,
 		dueDate: Date
 	) {
-		return this.requestFromBackend(authToken, 'post', 'ToDoItems', { title, description, dueDate })
+		return this._requestFromBackend(authToken, 'post', 'ToDoItems', { title, description, dueDate })
 	}
 
 	public updateToDoItem(
@@ -52,53 +53,57 @@ export class BackendService {
 		description: string,
 		dueDate: Date
 	) {
-		return this.requestFromBackend(authToken, 'put', `ToDoItems/${id}`, { title, description, dueDate })
+		return this._requestFromBackend(authToken, 'put', `ToDoItems/${id}`, { title, description, dueDate })
 	}
 
 	public completeToDoItem(
 		authToken: string | null,
 		id: string
 	) {
-		return this.requestFromBackend(authToken, 'post', `ToDoItems/${id}/Complete`)
+		return this._requestFromBackend(authToken, 'post', `ToDoItems/${id}/Complete`)
 	}
 
 	public undoCompleteToDoItem(
 		authToken: string | null,
 		id: string
 	) {
-		return this.requestFromBackend(authToken, 'post', `ToDoItems/${id}/Undo`)
+		return this._requestFromBackend(authToken, 'post', `ToDoItems/${id}/Undo`)
 	}
 
 	public hideToDoItem(
 		authToken: string | null,
 		id: string
 	) {
-		return this.requestFromBackend(authToken, 'post', `ToDoItems/${id}/Hide`)
+		return this._requestFromBackend(authToken, 'post', `ToDoItems/${id}/Hide`)
 	}
 
 	public showToDoItem(
 		authToken: string | null,
 		id: string
 	) {
-		return this.requestFromBackend(authToken, 'post', `ToDoItems/${id}/Show`)
+		return this._requestFromBackend(authToken, 'post', `ToDoItems/${id}/Show`)
 	}
 
-	private requestFromBackend(
+	private _requestFromBackend(
 		authToken: string | null,
 		method: string,
 		uri: string,
 		body: any = null,
 		params: HttpParams | { [param: string]: string | number | boolean | readonly (string | number | boolean)[]; } | undefined = undefined
 	) {
-		return this.httpClient.request(method, `${this.baseUrl}/${this.version}/${uri}`, { body: body, headers: this.composeHeaders(authToken), params: params })
+		return this.httpClient.request(method, `${this.baseUrl}/${this.version}/${uri}`, { body: body, headers: this._composeHeaders(authToken), params: params })
 	}
 
-	private composeHeaders(token: string | null): HttpHeaders {
+	private _composeHeaders(token: string | null): HttpHeaders {
 		let headers: HttpHeaders = new HttpHeaders();
 
 		if (token)
 			headers = headers.set('Authorization', `Bearer ${token}`)
 
 		return headers;
+	}
+
+	private _currentDate(): string {
+		return new DatePipe('en-US').transform(new Date(), 'YYYY-MM-dd') ?? "";
 	}
 }
